e3d11a6b6aa1350a4c4dec87feefcdd06e45b366
Daniel Moretti V <daniel.morettiv@gmail.com>
2020-08-19 02:14:12 -0400

    Update guillepollo.sh

--- guillepollo.sh
+++ guillepollo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman -S xorg mesa lib32-mesa xf86-video-ati mesa-vdpau lib32-mesa-vdpau plasma-meta ttf-joypixels libreoffice-fresh firefox kde-graphics-meta kde-multimedia-meta kde-sdk-meta unrar unzip kde-system-meta ark-filelight kate code inkscape blender gimp steam kcalc kcharselect kdialog konsole ktimer print-manager cups pavucontrol wine-staging winetricks
+pacman -S xorg mesa lib32-mesa xf86-video-ati mesa-vdpau lib32-mesa-vdpau plasma-meta ttf-joypixels libreoffice-fresh firefox kde-graphics-meta kde-multimedia-meta kde-sdk-meta unrar unzip kde-system-meta ark filelight kate code inkscape blender gimp steam kcalc kcharselect kdialog konsole ktimer print-manager cups pavucontrol wine-staging winetricks
