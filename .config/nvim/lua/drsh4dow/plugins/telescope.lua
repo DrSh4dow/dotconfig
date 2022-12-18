edbbf253222c44e29bb62c91d4ad1beab6a19b35
drsh4dow <daniel.morettiv@gmail.com>
2022-12-18 13:49:50 -0300

    customized some nvim configs regarding telescope and nvim-tree

--- .config/nvim/lua/drsh4dow/plugins/telescope.lua
+++ .config/nvim/lua/drsh4dow/plugins/telescope.lua
@@ -1,23 +1,30 @@
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-    return
+	return
 end
 
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-    return
+	return
 end
 
 telescope.setup({
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            }
-        }
-    }
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+			file_ignore_patterns = { "node_modules", ".git", ".next" },
+			no_ignore = true,
+		},
+	},
 })
 
 telescope.load_extension("fzf")
