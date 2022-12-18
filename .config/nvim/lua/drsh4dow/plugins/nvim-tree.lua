bd52ddc563f6b0eb8aa31e6f8f6f7615ec99006d
drsh4dow <daniel.morettiv@gmail.com>
2022-12-18 13:49:50 -0300

    customized some nvim configs regarding telescope and nvim-tree

--- .config/nvim/lua/drsh4dow/plugins/nvim-tree.lua
+++ .config/nvim/lua/drsh4dow/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-    return
+	return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -10,21 +10,28 @@ vim.g.loaded_netrwPlugin = 1
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 nvimtree.setup({
-    renderer = {
-        icons = {
-            glyphs = {
-                folder = {
-                    arrow_closed = "➜",
-                    arrow_open = "↓",
-                },
-            },
-        },
-    },
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false,
-            },
-        },
-    },
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "➜",
+					arrow_open = "↓",
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	filters = {
+		dotfiles = false,
+	},
+	git = {
+		enable = true,
+		ignore = false,
+	},
 })
