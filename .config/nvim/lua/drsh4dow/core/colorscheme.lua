c2bf91ed1eae7ef2ac55cf014dba9f4df732f1c4
drsh4dow <daniel.morettiv@gmail.com>
2022-12-20 23:27:56 -0300

    removed mpv folder

--- .config/nvim/lua/drsh4dow/core/colorscheme.lua
+++ .config/nvim/lua/drsh4dow/core/colorscheme.lua
@@ -6,4 +6,5 @@ end
 
 -- transparent windows
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
