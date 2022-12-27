1e9be3be59b92225c311115570150222b229e08d
drsh4dow <daniel.morettiv@gmail.com>
2022-12-27 13:54:35 -0300

    refactored some stuff

--- .config/nvim/lua/drsh4dow/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/drsh4dow/plugins/lsp/null-ls.lua
@@ -30,20 +30,6 @@ null_ls.setup({
 	on_attach = function(current_client, bufnr)
 		if current_client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						timeout_ms = 5000,
-						filter = function(client)
-							--  only use null-ls for formatting instead of lsp server
-							return client.name == "null-ls"
-						end,
-						bufnr = bufnr,
-					})
-				end,
-			})
 		end
 	end,
 })
