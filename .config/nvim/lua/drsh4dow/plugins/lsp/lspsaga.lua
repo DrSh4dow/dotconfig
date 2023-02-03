d2a1e1ce58a7dfb29dea996fcfb4f4a69f58f740
drsh4dow <daniel.morettiv@gmail.com>
2023-02-02 22:47:32 -0300

    nvim changes

--- .config/nvim/lua/drsh4dow/plugins/lsp/lspsaga.lua
+++ .config/nvim/lua/drsh4dow/plugins/lsp/lspsaga.lua
@@ -3,7 +3,7 @@ if not saga_status then
 	return
 end
 
-saga.init_lsp_saga({
+saga.setup({
 	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
 	finder_action_keys = { open = "<CR>" },
 	definition_action_keys = { edit = "<CR>" },
