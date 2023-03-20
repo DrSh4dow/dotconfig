cd4f527503c795c96238fbd260fb10ae4154a516
Daniel Moretti <daniel.morettiv@gmail.com>
2023-03-19 22:28:02 -0300

    updated general configs

--- .config/nvim/lua/drsh4dow/plugins/lsp/mason.lua
+++ .config/nvim/lua/drsh4dow/plugins/lsp/mason.lua
@@ -30,6 +30,7 @@ mason_lspconfig.setup({
 		"svelte",
 		"emmet_ls",
 		"prismals",
+    "astro"
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
